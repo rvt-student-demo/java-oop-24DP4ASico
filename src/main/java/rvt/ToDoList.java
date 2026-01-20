bea17927f73df3908cad042e3ce7f05738997759
24DP4ASico <a240411as@rvt.lv>
2026-01-20 13:50:37 +0200

    To do list

--- src/main/java/rvt/ToDoList.java
+++ src/main/java/rvt/ToDoList.java
@@ -1,23 +1,68 @@
 package rvt;
+import java.io.File;
 import java.util.ArrayList;
 
 public class ToDoList {
-   private ArrayList<String> list;
+   private ArrayList<String> tasks;
+   private final String filePath = "todo.csv";
     public ToDoList() {
-         this.list = new ArrayList<>();
+         this.tasks = new ArrayList<>();
+            loadFromFile();
+    }
+
+    private void loadFromFile() {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return; 
+        }
+
+        try (java.util.Scanner fileScanner = new java.util.Scanner(file)) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                this.tasks.add(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error loading tasks from file: " + e.getMessage());
+        }
+        
+        
     }
     public void add(String task) {
-        this.list.add(task);
+        this.tasks.add(task);
+        updateFile();
     }
 
     public void print(){
-        for (String job: list){
-            System.out.println(this.list.indexOf(job)+ 1 + ":" + job);    
+        for (String job: tasks){
+            System.out.println(this.tasks.indexOf(job)+ 1 + ":" + job);    
         }
     }
 
     public void remove(int number){
-        this.list.remove(number - 1);
+        this.tasks.remove(number - 1);
+        updateFile();
+    }
+
+    public ArrayList<String> getTasks() {
+        return tasks;
+    }
+
+    public String getTask(int id){
+        if (id < 1 || id > tasks.size()) {
+            return null; 
+        }
+        return tasks.get(id);
+    }
+
+    private boolean updateFile() {
+        try (java.io.PrintWriter writer = new java.io.PrintWriter(filePath)) {
+            for (String task : tasks) {
+                writer.println(task);
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
       
