18ed48b037cc316eaf940814fe12650a84f495ca
24DP4ASico <a240411as@rvt.lv>
2026-03-03 12:47:36 +0200

    todolist

--- src/main/java/rvt/MainToDoList.java
+++ src/main/java/rvt/MainToDoList.java
@@ -2,14 +2,18 @@ package rvt;
 
 import java.util.Scanner;
 
-public class MainToDoList {
+public class MainToDoList{
     private static ToDoList todo;
+    public static void main(String[] args){
+        
+            ToDoList toDoList = new ToDoList();
+            Scanner scanner = new Scanner(System.in);
+            UserInteface ui = new UserInteface(toDoList, scanner);
+            ui.start(); 
 
-    public static void main(String[] args) {
-        ToDoList toDoList = new ToDoList();
-        Scanner scanner = new Scanner(System.in);
-        UserInteface ui = new UserInteface(toDoList, scanner);
-        ui.start(); 
-    }
     
+    
+    }
 }
+
+
