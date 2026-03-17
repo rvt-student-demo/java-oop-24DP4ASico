091ebb10fec342e107d87f1532e839509b42e011
24DP4ASico <a240411as@rvt.lv>
2026-03-17 13:09:17 +0200

    Online Shop part 4

--- src/main/java/rvt/ManeOS.java
+++ src/main/java/rvt/ManeOS.java
@@ -2,17 +2,11 @@ package rvt;
 
 public class ManeOS {
 public static void main(String[] args) {
-Warehouse warehouse = new Warehouse();
-warehouse.addProduct("milk", 3, 10);
-warehouse.addProduct("coffee", 5, 6);
-warehouse.addProduct("buttermilk", 2, 20);
-warehouse.addProduct("yogurt", 2, 20);
-
-System.out.println("products:");
-
-for (String product: warehouse.products()) {
-    System.out.println(product);
-}
+Item item = new Item("milk", 4, 2);
+System.out.println("an item that contains 4 milks has the total price of " + item.price());
+System.out.println(item);
+item.increaseQuantity();
+System.out.println(item);
 
 }
 }
