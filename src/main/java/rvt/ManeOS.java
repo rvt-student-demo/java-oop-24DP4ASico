266eaf9ee631ece782b2ce4ff2e8821842850a54
24DP4ASico <a240411as@rvt.lv>
2026-03-17 13:24:47 +0200

    Online shop part 7

--- src/main/java/rvt/ManeOS.java
+++ src/main/java/rvt/ManeOS.java
@@ -4,12 +4,19 @@ public class ManeOS {
 public static void main(String[] args) {
 ShopingCart cart = new ShopingCart();
 cart.add("milk", 3);
-cart.add("buttermilk", 2);
-cart.add("cheese", 5);
-System.out.println("cart price: " + cart.price());
-cart.add("computer", 899);
-System.out.println("cart price: " + cart.price());
-
 cart.print();
+System.out.println("cart price: " + cart.price() + "\n");
+
+cart.add("buttermilk", 2);
+cart.print();
+System.out.println("cart price: " + cart.price() + "\n");
+
+cart.add("milk", 3);
+cart.print();
+System.out.println("cart price: " + cart.price() + "\n");
+
+cart.add("milk", 3);
+cart.print();
+System.out.println("cart price: " + cart.price() + "\n");
 }
 }
