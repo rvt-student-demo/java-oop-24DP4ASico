97fa67e2f60e7a8f9a53db03efa1c67007fb5915
24DP4ASico <a240411as@rvt.lv>
2026-03-17 11:40:23 +0200

    I owe you

---
+++ src/main/java/rvt/MainIOU.java
@@ -0,0 +1,14 @@
+package rvt;
+
+
+public class MainIOU {
+
+    public static void main(String[] args) {
+
+Ioweu mattsIOU = new Ioweu();
+mattsIOU.setSum("Arthur", 51.5);
+mattsIOU.setSum("Arthur", 10.5);
+
+System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+    }
+}
