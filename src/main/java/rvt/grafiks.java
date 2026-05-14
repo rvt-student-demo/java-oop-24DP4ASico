6eb532101043af4f14d8930349090b122628be90
24DP4ASico <a240411as@rvt.lv>
2026-05-14 10:54:15 +0300

    grafiks

---
+++ src/main/java/rvt/grafiks.java
@@ -0,0 +1,16 @@
+package rvt;
+
+import javax.swing.JFrame;
+
+public class grafiks {
+
+    public static void main(String[] args) {
+       
+        JFrame frame = new JFrame("Grafiks");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        
+    }
+    
+}
